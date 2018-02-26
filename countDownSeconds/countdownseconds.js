bf3f7ad128b963a5eeb9ad0145170613f7e2bb89
Marcel Petrick <mail@marcelpetrick.it>
2018-02-26 13:13:31 +0100

    20180226: added a function to translate inputnumbers to colors; on button B the colors are cycled on the LED

--- countDownSeconds/countdownseconds.js
+++ countDownSeconds/countdownseconds.js
@@ -7,8 +7,11 @@ input.onButtonPressed(Button.A, () => {
 })
 input.onButtonPressed(Button.B, () => {
     // basic.showString(control.deviceName()) cycle colors
-    currentColor += 1
-    basic.setLedColor(currentColor)
+    //increase and cap to 0..3
+
+    //currentColor = (currentColor + 1) % 8;
+    //basic.setLedColor(translateColor(currentColor))
+    cycleColorsForever();
 })
 basic.forever(() => {
     if (activated > 0) {
@@ -24,3 +27,29 @@ basic.forever(() => {
         basic.setLedColor(Colors.Red)
     }
 })
+
+function cycleColorsForever(): void {
+    currentColor = 0
+    while (true) {
+        currentColor = (currentColor + 1) % 8;
+        basic.setLedColor(translateColor(currentColor))
+        basic.pause(500)
+    }
+}
+function translateColor(inputNumber: number): number {
+    let returnValue = Colors.Red;
+
+    switch (inputNumber) {
+        case 0: returnValue = Colors.Red; break;
+        case 1: returnValue = Colors.Blue; break;
+        case 2: returnValue = Colors.Green; break;
+        case 3: returnValue = Colors.Yellow; break;
+        case 4: returnValue = Colors.Indigo; break;
+        case 5: returnValue = Colors.Orange; break;
+        case 6: returnValue = Colors.Purple; break;
+        case 7: returnValue = Colors.Violet; break;
+        default:; //do nothing
+    }
+
+    return returnValue;
+}
