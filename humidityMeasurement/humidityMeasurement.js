1605379d89ad0b4f74de5e618a575d9a47fa54ee
Marcel Petrick <mail@marcelpetrick.it>
2018-03-17 23:17:18 +0100

    20180317: added the first try for some longtime humidity-measurement with min and max values and accounting of the samples

---
+++ humidityMeasurement/humidityMeasurement.js
@@ -0,0 +1,32 @@
+let v_sampleSize = 0
+let v_maxMeasurement = 0
+let currentValue = 0
+let v_minMeasurement = 0
+let v_sleepTime = 0
+basic.forever(() => {
+    basic.pause(v_sleepTime)
+    currentValue = pins.analogReadPin(AnalogPin.P2)
+    if (currentValue > v_maxMeasurement) {
+        v_maxMeasurement = currentValue
+    }
+    if (currentValue < v_minMeasurement) {
+        v_minMeasurement = currentValue
+    }
+    v_sampleSize += 1
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.A, () => {
+    basic.showString("min:" + v_minMeasurement + ("max:" + v_maxMeasurement) + ("samples:" + v_sampleSize))
+})
+v_sleepTime = 60 * 1000
+v_minMeasurement = 1024
+v_maxMeasurement = -1
+v_sampleSize = 0
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
+
