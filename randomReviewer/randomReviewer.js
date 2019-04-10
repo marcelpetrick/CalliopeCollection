c4fcae99abe98befe30e95e3f35878c7d7a2d83b
Marcel Petrick <mail@marcelpetrick.it>
2019-04-10 11:31:27 +0200

    randomReviewer: show environment-values between the runs; changed the tunes
    Change-Id: If1a67073b95fe56c4a02e29ecfbc13071785cbdc

--- randomReviewer/randomReviewer.js
+++ randomReviewer/randomReviewer.js
@@ -1,23 +1,10 @@
 //javascript for the calliope editor
 // found at: https://makecode.calliope.cc/
 
+let item = 0
 let reviewer = ""
 let namesAll: string[] = []
 let namesInternal: string[] = []
-input.onButtonPressed(Button.B, () => {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
-    reviewer = namesAll[Math.random(namesAll.length + 1)]
-    for (let Platzhalter = 0; Platzhalter <= 2; Platzhalter++) {
-        basic.showString(reviewer)
-    }
-})
 input.onButtonPressed(Button.A, () => {
     basic.showLeds(`
         . . . . .
@@ -26,9 +13,23 @@ input.onButtonPressed(Button.A, () => {
         . . . . .
         . . . . .
         `)
-    music.beginMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+    music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
     reviewer = namesInternal[Math.random(namesInternal.length + 1)]
     for (let Platzhalter2 = 0; Platzhalter2 <= 2; Platzhalter2++) {
+        basic.showString(reviewer)
+    }
+})
+input.onButtonPressed(Button.B, () => {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+    reviewer = namesAll[Math.random(namesAll.length + 1)]
+    for (let Platzhalter = 0; Platzhalter <= 2; Platzhalter++) {
         basic.showString(reviewer)
     }
 })
@@ -37,18 +38,8 @@ namesAll = namesInternal
 namesAll.push("RNI")
 namesAll.push("NLE")
 basic.forever(() => {
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.showString("lumi:" + input.lightLevel())
+    basic.showString("heading:" + input.compassHeading())
+    basic.showString("temp:" + input.temperature())
+    item += Math.random(namesAll.length + 1)
 })
