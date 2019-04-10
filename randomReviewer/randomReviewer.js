03d4b5d7a9bd0c35f8b601f73da37f9bc3e797cb
Marcel Petrick <mail@marcelpetrick.it>
2019-04-10 12:12:27 +0200

    randomReviewer: simplified
    Change-Id: I64889a4e4d3072e83b177eb87e466013f960ccdf

--- randomReviewer/randomReviewer.js
+++ randomReviewer/randomReviewer.js
@@ -1,9 +1,11 @@
 //javascript for the calliope editor
 // found at: https://makecode.calliope.cc/
+
+let namesAll: string[] = []
 let item = 0
 let reviewer = ""
-let namesAll: string[] = []
 let namesInternal: string[] = []
+let Platzhalter = 0
 input.onButtonPressed(Button.A, () => {
     basic.showLeds(`
         . . . . .
@@ -12,7 +14,6 @@ input.onButtonPressed(Button.A, () => {
         . . . . .
         . . . . .
         `)
-    music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
     reviewer = namesInternal[Math.random(namesInternal.length + 1)]
     for (let Platzhalter2 = 0; Platzhalter2 <= 2; Platzhalter2++) {
         basic.showString(reviewer)
@@ -26,7 +27,6 @@ input.onButtonPressed(Button.B, () => {
         . . . . .
         . . . . .
         `)
-    music.beginMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
     reviewer = namesAll[Math.random(namesAll.length + 1)]
     for (let Platzhalter = 0; Platzhalter <= 2; Platzhalter++) {
         basic.showString(reviewer)
@@ -37,8 +37,8 @@ namesAll = namesInternal
 namesAll.push("RNI")
 namesAll.push("NLE")
 basic.forever(() => {
+    for (let Platzhalter = 0; Platzhalter <= 8; Platzhalter++) {
+        item += Math.random(namesAll.length + 1)
+    }
     basic.showString("lumi" + input.lightLevel())
-    basic.showString("hdg" + input.compassHeading())
-    basic.showString("temp" + input.temperature())
-    item += Math.random(namesAll.length + 1)
 })
