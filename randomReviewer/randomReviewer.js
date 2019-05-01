b74519bfd9e047a4fe620c0fa4d28d6a8378171d
mail@marcelpetrick.it <mail@marcelpetrick.it>
2019-05-01 23:45:16 +0200

    randomReviewer: rewrote the code and simplified it. No distinction between internals and externals anymore. Removed the power-consuming display of the temperature. Just repeat the chosen reviewer once.

--- randomReviewer/randomReviewer.js
+++ randomReviewer/randomReviewer.js
@@ -2,23 +2,15 @@
 // found at: https://makecode.calliope.cc/
 
 let reviewer = ""
-let namesInternal: string[] = []
 input.onButtonPressed(Button.A, () => {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    music.setTempo(120)
-    music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-    reviewer = namesInternal[Math.random(namesInternal.length + 1)]
-    for (let Platzhalter2 = 0; Platzhalter2 <= 2; Platzhalter2++) {
-        basic.showString(reviewer)
-    }
+    findAndShowReviewer()
 })
+
 input.onButtonPressed(Button.B, () => {
+    findAndShowReviewer()
+})
+
+function findAndShowReviewer()  {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -29,18 +21,13 @@ input.onButtonPressed(Button.B, () => {
     music.setTempo(120)
     music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
     reviewer = namesAll[Math.random(namesAll.length + 1)]
-    for (let Platzhalter3 = 0; Platzhalter3 <= 2; Platzhalter3++) {
-        basic.showString(reviewer)
-    }
-})
+    basic.showString(reviewer)
+}
 let namesAll: string[] = []
-namesInternal = ["GSC", "HGA", "MSA", "NKU", "MDR", "MPE", "HCH"]
-namesAll = namesInternal
-namesAll.push("RNI")
-namesAll.push("NLE")
+namesAll = ["GSC", "HGA", "NKU", "MDR", "MPE", "HCH", "RNI", "NLE"]
 basic.forever(() => {
-    for (let i = 0; i < 32; i++) {
+    for (let i = 0; i < 31; i++) {
         Math.random(namesAll.length + 1)
     }
-    basic.showString("" + input.temperature() + "C")
+    control.waitMicros(1000)
 })
