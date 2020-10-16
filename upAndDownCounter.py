7b22ee6ab6b5dc84ad4f78615ec2896e11aad83a
Marcel Petrick <mail@marcelpetrick.it>
2020-10-16 23:58:36 +0200

    upAndDownCounter.py: added code; forgot the usb-cable, so purely software-sim
    Smol program for my lovely daughter. To have some fun with the buttons.
    A counts up, B down. Range is 0..9.

---
+++ upAndDownCounter.py
@@ -0,0 +1,17 @@
+def on_button_pressed_a():
+    global value
+    value += 1
+    value = value % 10
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global value
+    value -= 1
+    value = (value+10) % 10
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+value = 1
+
+def on_forever():
+    basic.show_number(value)
+basic.forever(on_forever)
