877c00a955785e0145e9795270c0d2e6c360760f
Marcel Petrick <mail@marcelpetrick.it>
2020-10-22 16:23:36 +0200

    Update teatimer.py

--- teatimer.py
+++ teatimer.py
@@ -3,8 +3,9 @@
 def on_button_pressed_a():
     global duration
     if programState == 0:
-        duration += 1
-        duration = duration % maxDuration
+        duration = (duration + 1) % maxDuration
+        if duration == 0:
+            duration = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
@@ -12,9 +13,6 @@ def on_button_pressed_b():
     programState += 1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-duration = 0
-maxDuration = 0
-programState = 0
 programState = 0
 maxDuration = 5
 duration = 1
@@ -22,9 +20,8 @@ duration = 1
 def on_forever():
     if programState == 0:
         basic.show_number(duration)
+    elif programState == 1:
+        pass
     else:
-        if programState == 1:
-            pass
-        else:
-            pass
+        pass
 basic.forever(on_forever)
