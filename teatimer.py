c8b1c844927fed87488f4af6c596a9e29dfaa0e1
Marcel Petrick <mail@marcelpetrick.it>
2020-10-22 17:35:43 +0200

    Update teatimer.py

--- teatimer.py
+++ teatimer.py
@@ -1,4 +1,9 @@
 # wip; just a configurable, fashy tea timer
+#
+# timer has several states:
+# 1st: (set the time by pressing A; just increases up to 4 seconds); start the timer with B
+# 2nd: counts down the time (and shows the remaineder as scrolled text (this causes a delay.. uff)
+# 3rd: time elapsed -> blinkng red/green; can be reset by pressing B
 
 def on_button_pressed_a():
     global duration
@@ -10,29 +15,35 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global programState, startTime
-    programState += 1
-    startTime = control.millis()
+    if programState == 0 or programState == 2:
+        programState = (programState + 1) % maxProgramState
+        startTime = control.millis()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 startTime = 0
 programState = 0
+maxProgramState = 3
 maxDuration = 5
 duration = 1
 basic.turn_rgb_led_off()
 
 def on_forever():
     global programState
-    global startTime
-    global duration
     if programState == 0:
+        basic.set_led_color(0x00ff00)
         basic.show_number(duration)
     elif programState == 1:
+        basic.set_led_color(0x0000ff)
         currentTime = control.millis()
-        # time in milliseconds
-        remainingTime = duration - ((currentTime - startTime) / 1000)
-        basic.show_number(remainingTime)
-        if currentTime < 0:
+        remainingTimeMs = (1000 * duration - (currentTime - startTime))
+        remainingTimeS = remainingTimeMs / 1000
+        basic.show_number(remainingTimeS )
+        if remainingTimeS <= 0:
             programState = 2
     elif programState == 2:
-        pass
+        basic.set_led_color(0xff0000)
+        basic.pause(100)
+        basic.set_led_color(0x00ff00)
+        basic.pause(100)
 basic.forever(on_forever)
+
