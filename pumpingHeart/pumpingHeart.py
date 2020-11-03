f8449d9de1822936616a7658b4675826fc68e0cf
Marcel Petrick <mail@marcelpetrick.it>
2020-11-04 00:57:52 +0100

    pumpingHeart: fixed; setting LED-brightness over 255 is not good

--- pumpingHeart/pumpingHeart.py
+++ pumpingHeart/pumpingHeart.py
@@ -1,18 +1,21 @@
-factor = 0.5
-b = 255
+# pumping heart on the LED-matrix: from full brightness to "off" and back ..
+
+factor = 2
+b = 1
 led.set_brightness(b)
 
 while not (input.button_is_pressed(Button.A)):
-    b = led.brightness() * factor
-    led.set_brightness(b)
+    b = b * factor    
     
-    if led.brightness() <= 1:
+    if b <= 1:
         b = 1
         factor = 2
 
-    if led.brightness() >= 255:
+    if b >= 255:
         b = 255
         factor = 0.5
+
+    led.set_brightness(b)
     
     basic.show_icon(IconNames.HEART)
     #basic.pause(10)
