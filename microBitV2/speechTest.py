802b85129c201ffa1e01238df9975b468fe20589
Marcel Petrick <mail@marcelpetrick.it>
2021-01-04 23:12:50 +0100

    micro:bit v2: speech synthesis test: shifting channels; final version

--- microBitV2/speechTest.py
+++ microBitV2/speechTest.py
@@ -3,15 +3,26 @@ import random
 import microbit
 
 def sayText():
-    text = ["Useless climate, staring at me", "Yearning, yearning to be free", "The unpredictable sense of a short road", "An epidemic that comprehends"]
+    # from the docu at https://microbit-micropython.readthedocs.io/en/v1.0.1/tutorials/speech.html
+    # pitch - how high or low the voice sounds (0 = high, 255 = Barry White)
+    # speed - how quickly the device talks (0 = impossible, 255 = bedtime story)
+    # mouth - how tight-lipped or overtly enunciating the voice sounds (0 = ventriloquist’s dummy, 255 = Foghorn Leghorn)
+    # throat - how relaxed or tense is the tone of voice (0 = falling apart, 255 = totally chilled)
+
+    text = ["Useless climate, staring at me",
+    "Yearning, yearning to be free",
+    "The unpredictable sense of a short road",
+    "An epidemic that comprehends"]
     
     for line in text:
-        speech.say(line, speed=120, pitch=100, throat=100, mouth=200)
+        speech.say(line, speed=110, pitch=200, throat=50, mouth=200)
+        microbit.sleep(333)
 
 while True:
     if microbit.button_a.is_pressed():
-        microbit.display.scroll("A")
-        sayText()        
+        #display.show(Image.SAD)
+        sayText()
+        #display.clear()
     elif microbit.button_b.is_pressed():
         microbit.display.scroll("B")
         
