add278895b2c731a0dd35bae444240c8ca4571b6
Marcel Petrick <mail@marcelpetrick.it>
2021-01-04 22:44:59 +0100

    micro:bit v2: speech synthesis test: DALEK, haha

--- microBitV2/speechTest.py
+++ microBitV2/speechTest.py
@@ -1,4 +1,5 @@
 import speech
 
 while True:
-    speech.say("Hey ho, let's go.")
+    #speech.say("Hey ho, let's go.")
+    speech.say("I am a DALEK - EXTERMINATE", speed=120, pitch=100, throat=100, mouth=200)
