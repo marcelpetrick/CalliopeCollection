3fb5c1d43af1095369ab7b4e9c93e075b2e7ea99
Marcel Petrick <mail@marcelpetrick.it>
2021-01-04 22:52:47 +0100

    micro:bit v2: speech synthesis test: shifting channels

--- microBitV2/speechTest.py
+++ microBitV2/speechTest.py
@@ -1,5 +1,20 @@
 import speech
+import random
+import microbit
+
+text = "Useless climate, staring at me"
+    "Yearning, yearning to be free"
+    "The unpredictable sense of a short road"
+    "An epidemic that comprehends"
 
 while True:
-    #speech.say("Hey ho, let's go.")
-    speech.say("I am a DALEK - EXTERMINATE", speed=120, pitch=100, throat=100, mouth=200)
+    if microbit.button_a.is_pressed() and microbit.button_b.is_pressed():
+        microbit.display.scroll("AB")
+        break
+    elif microbit.button_a.is_pressed():
+        microbit.display.scroll("A")
+        speech.say(line, speed=120, pitch=100, throat=100, mouth=200)
+    elif microbit.button_b.is_pressed():
+        microbit.display.scroll("B")
+        
+    microbit.sleep(100)
